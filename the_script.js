23503c4008e23d37910f15964c6ed42971c7f523
Guilherme Vieira <super.driver.512@gmail.com>
2014-05-31 02:29:44 -0300

    WIP.

--- the_script.js
+++ the_script.js
@@ -1,20 +1,64 @@
-Array.prototype.merge = function(array_or_element) {
-    if(Array.isArray(array_or_element)) {
-        this.push.apply(this, array_or_element);
-    }
-    else {
-        this.push(array_or_element);
-    }
-    return this;
-};
+(function extend_primitives() {
+    Array.prototype.merge = function(array_or_element) {
+        if(Array.isArray(array_or_element)) {
+            this.push.apply(this, array_or_element);
+        }
+        else {
+            this.push(array_or_element);
+        }
+        return this;
+    };
+    Array.MergeData = function(data) {
+        this.data = data;
+    };
+    Array.prototype.map = function(fn) {
+        var result = [];
+        this.forEach (
+            function(element, index) {
+                var mapped_element = fn(element, index);
+                if(mapped_element.constructor === Array.MergeData) {
+                    result.merge(mapped_element.data);
+                }
+                else {
+                    result.push(mapped_element);
+                }
+            }
+        );
+        return result;
+    };
+})();
 midi.note_on = 144;
 midi.note_off = 128;
-function make_note(note, moment, length = null, strike_velocity = null, off_velocity = null) {
-    length = length || 0.2;
-    strike_velocity = strike_velocity || 127;
-    off_velocity = off_velocity || 127;
+midi.bpm = 120;
+midi.quarter_length = 60 / midi.bpm;
+function make_note(note, moment = null, length = null) {
+    moment = moment || 0;
+    length = length || midi.quarter_length;
     return [
-        { moment: moment, type: midi.note_on, note: note, velocity: strike_velocity }
-        , { moment: moment + length, type: midi.note_off, note: note, velocity: off_velocity }
+        { moment: moment, type: midi.note_on, note: note, velocity: 127 }
+        , { moment: moment + length, type: midi.note_off, note: note, velocity: 127 }
     ];
 }
+function make_melody() {
+    var melody = [].slice.call(arguments, 0);
+    return melody.map (
+        function(note, index) {
+            var length;
+            var length_divisor = 1;
+            if(Array.isArray(note)) {
+                length = note[1] || length;
+                if(!note[0]) {
+                    note = note[0];
+                }
+                else {
+                    throw new Error("Invalid note: " + JSON.stringify(note));
+                }
+                note = note[0];
+            }
+            return make_note(note, midi.quarter_length / length_divisor);
+        }
+    );
+}
+(function test_stuff() {
+    midi.play([].merge(make_note(51, 0)).merge(make_note(53, 0.2)).merge(make_note(54, 0)).merge(make_note(58, 0.4)))
+})();
